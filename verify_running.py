fdde3b371ab7c252e3ec92498031600c60b16352
Aaron Coburn <acoburn@apache.org>
2018-12-13 17:47:45 -0500

    Testing

---
+++ verify_running.py
@@ -0,0 +1,12 @@
+import requests
+import backoff
+
+@backoff.on_exception(backoff.expo,
+        requests.exceptions.RequestException,
+        max_time=60)
+def get_url(url):
+    return requests.get(url)
+
+if __name__ == "__main__":
+    get_url("http://localhost/")
+    get_url("http://localhost:8080/")
