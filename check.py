548ef18582040e7402261b9ce1cc0d26f5284913
Aaron Coburn <acoburn@apache.org>
2018-12-13 09:25:51 -0500

    Adjust startup values

--- check.py
+++ check.py
@@ -2,6 +2,6 @@ import requests
 
 if __name__ == "__main__":
     print("Starting")
-    r = requests.get("http://localhost/")
+    r = requests.get("http://localhost:8080/")
     print(r.status_code)
     print(r.headers['content-type'])
