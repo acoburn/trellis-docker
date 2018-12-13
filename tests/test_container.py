d2a30f2d690e572640eff0ee26a841ed6ef92e89
Aaron Coburn <acoburn@apache.org>
2018-12-13 17:43:44 -0500

    remove hard-coded URL

--- tests/test_container.py
+++ tests/test_container.py
@@ -2,8 +2,6 @@ import pytest
 import requests
 from requests.utils import parse_header_links
 
-ROOT = "http://localhost:8080/";
-
 
 def test_get_container(baseurl):
     r = requests.get(baseurl)
@@ -15,17 +13,17 @@ def test_get_container(baseurl):
     assert "http://www.w3.org/ns/ldp#BasicContainer" in types
     assert "http://www.w3.org/ns/ldp#Resource" in types
 
-def test_get_json_ld():
-    r = requests.get(ROOT, headers={"accept": "application/ld+json"})
+def test_get_json_ld(baseurl):
+    r = requests.get(baseurl, headers={"accept": "application/ld+json"})
     assert 200 == r.status_code
     assert "application/ld+json" == r.headers['content-type']
 
-def test_post_binary():
+def test_post_binary(baseurl):
     headers = {
             "content-type": "text/plain",
             "link": "<http://www.w3.org/ns/ldp#NonRDFSource>; rel='type'"}
     data = "A simple file."
-    r = requests.post(ROOT, headers=headers, data=data)
+    r = requests.post(baseurl, headers=headers, data=data)
     assert 201 == r.status_code
     location = r.headers['location']
 
@@ -34,16 +32,16 @@ def test_post_binary():
     assert "text/plain" == r.headers['content-type']
     assert data == r.text
 
-    r = requests.get(ROOT, headers={"accept":"application/ld+json"})
+    r = requests.get(baseurl, headers={"accept":"application/ld+json"})
     d = r.json()
     assert location in d['contains']
 
-def test_post_rdf():
+def test_post_rdf(baseurl):
     headers = {
             "content-type": "text/turtle",
             "link": "<http://www.w3.org/ns/ldp#RDFSource>; rel='type'"}
     data = "<> a <http://example.com/TestResource> ."
-    r = requests.post(ROOT, headers=headers, data=data)
+    r = requests.post(baseurl, headers=headers, data=data)
     assert 201 == r.status_code
     location = r.headers['location']
 
@@ -53,7 +51,7 @@ def test_post_rdf():
     d = r.json()
     assert "http://example.com/TestResource" == d['@type']
 
-    r = requests.get(ROOT, headers={"accept":"application/ld+json"})
+    r = requests.get(baseurl, headers={"accept":"application/ld+json"})
     d = r.json()
     assert location in d['contains']
 
