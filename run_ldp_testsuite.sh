e3d146e4e476a63673d5c9a392b085c90fe4cc01
Aaron Coburn <acoburn@apache.org>
2019-06-03 17:13:57 -0400

    Fix jsonld file output

--- run_ldp_testsuite.sh
+++ run_ldp_testsuite.sh
@@ -8,7 +8,7 @@ curl -s http://127.0.0.1/ -XPOST -H"Slug: basic" -H"Link: <http://www.w3.org/ns/
 java -jar ldp-testsuite.jar --software "Trellis LDP" --server http://127.0.0.1/basic --basic --non-rdf --earl --software "Trellis Linked Data Server" --shortname "Trellis" --language "Java" --homepage https://www.trellisldp.org --assertor https://www.trellisldp.org --developer "Aaron Coburn" | grep -v DEBUG
 mv report/ldp-testsuite-execution-report.html report/basic.html
 mv report/ldp-testsuite-execution-report-earl.ttl report/basic.ttl
-mv report/ldp-testsuire-execution-report-earl.jsonld report/basic.json
+mv report/ldp-testsuite-execution-report-earl.jsonld report/basic.json
 
 # Create a direct container
 curl -s http://127.0.0.1/ -XPOST -H"Slug: member_dc" -H"Link: <http://www.w3.org/ns/ldp#RDFSource>; rel=\"type\"" -H"Content-Type: text/turtle" --data-binary @resources/member.ttl
