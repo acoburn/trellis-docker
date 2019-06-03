d71a9a6bae39d99b1106eb3eb8c8c54be093768a
Aaron Coburn <acoburn@apache.org>
2019-06-03 17:53:52 -0400

    Better content types for reports

--- run_ldp_testsuite.sh
+++ run_ldp_testsuite.sh
@@ -8,7 +8,7 @@ curl -s http://127.0.0.1/ -XPOST -H"Slug: basic" -H"Link: <http://www.w3.org/ns/
 java -jar ldp-testsuite.jar --software "Trellis LDP" --server http://127.0.0.1/basic --basic --non-rdf --earl --software "Trellis Linked Data Server" --shortname "Trellis" --language "Java" --homepage https://www.trellisldp.org --assertor https://www.trellisldp.org --developer "Aaron Coburn" | grep -v DEBUG
 mv report/ldp-testsuite-execution-report.html report/basic.html
 mv report/ldp-testsuite-execution-report-earl.ttl report/basic.ttl
-mv report/ldp-testsuite-execution-report-earl.jsonld report/basic.jsonld
+mv report/ldp-testsuite-execution-report-earl.jsonld report/basic.json
 
 # Create a direct container
 curl -s http://127.0.0.1/ -XPOST -H"Slug: member_dc" -H"Link: <http://www.w3.org/ns/ldp#RDFSource>; rel=\"type\"" -H"Content-Type: text/turtle" --data-binary @resources/member.ttl
@@ -16,7 +16,7 @@ curl -s http://127.0.0.1/ -XPOST -H"Slug: direct" -H"Link: <http://www.w3.org/ns
 java -jar ldp-testsuite.jar --software "Trellis LDP" --server http://127.0.0.1/direct --direct --non-rdf --earl --software "Trellis Linked Data Server" --shortname "Trellis" --language "Java" --homepage https://www.trellisldp.org --assertor https://www.trellisldp.org --developer "Aaron Coburn" | grep -v DEBUG
 mv report/ldp-testsuite-execution-report.html report/direct.html
 mv report/ldp-testsuite-execution-report-earl.ttl report/direct.ttl
-mv report/ldp-testsuite-execution-report-earl.jsonld report/direct.jsonld
+mv report/ldp-testsuite-execution-report-earl.jsonld report/direct.json
 
 # Create an indirect container
 curl -s http://127.0.0.1/ -XPOST -H"Slug: member_ic" -H"Link: <http://www.w3.org/ns/ldp#RDFSource>; rel=\"type\"" -H"Content-Type: text/turtle" --data-binary @resources/member.ttl
@@ -24,5 +24,5 @@ curl -s http://127.0.0.1/ -XPOST -H"Slug: indirect" -H"Link: <http://www.w3.org/
 java -jar ldp-testsuite.jar --software "Trellis LDP" --server http://127.0.0.1/indirect --indirect --non-rdf --earl --software "Trellis Linked Data Server" --shortname "Trellis" --language "Java" --homepage https://www.trellisldp.org --assertor https://www.trellisldp.org --developer "Aaron Coburn" | grep -v DEBUG
 mv report/ldp-testsuite-execution-report.html report/indirect.html
 mv report/ldp-testsuite-execution-report-earl.ttl report/indirect.ttl
-mv report/ldp-testsuite-execution-report-earl.jsonld report/indirect.jsonld
+mv report/ldp-testsuite-execution-report-earl.jsonld report/indirect.json
 
